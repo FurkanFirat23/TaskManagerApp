dc84de763d90d507a7f3632c95daaab576a063ac
Furkan Fırat <furkan.0096@gmail.com>
2023-06-06 16:15:56 +0300

    sidebar added to dashboard layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,12 +1,14 @@
 import { GlassPane } from '@/components/GlassPane'
+import Sidebar from '@/components/Sidebar'
 
 import "@/styles/global.css"
 function DashboardLayout({children}) {
   return (
     <html lang='en'>
         <head/>
-        <body  className="h-screen w-screen rainbow-mesh p-6">
+        <body  className="h-screen w-screen candy-mesh p-6">
     <GlassPane className="w-full h-full flex items-center justify-center ">
+      <Sidebar/>
     {children}
     </GlassPane>
         </body>
